09acae9cd5bba563846c8502724a725831f92e21
austinzhai <austin.zhai@hotmail.com>
2026-04-13 09:54:14 +0800

    绑定自定义域名www.zhaiweidong.com

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://www.zhaiweidong.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
